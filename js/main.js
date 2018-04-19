6130f4b9d114fc920e5cb104dc882e31e9e77969
anastasiaprh <anastasiyaprh@gmail.com>
2018-04-19 18:03:19 +0300

    update

--- js/main.js
+++ js/main.js
@@ -2,6 +2,14 @@
 
 $(function() {
 
+  lottie.loadAnimation({
+  container: document.getElementById('bm'), // the dom element that will contain the animation
+  renderer: 'svg',
+  loop: true,
+  autoplay: true,
+  path: '..data.json' // the path to the animation json
+  });
+
   var didScroll;
   var lastScrollTop = 0;
   var delta = 5;
