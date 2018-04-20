cea02d067ea7bc514110220813009657017c8ced
anastasiaprh <anastasiyaprh@gmail.com>
2018-04-20 23:17:25 +0300

    update

--- js/main.js
+++ js/main.js
@@ -2,13 +2,13 @@
 
 $(function() {
 
-  lottie.loadAnimation({
-  container: document.getElementById('bm'), // the dom element that will contain the animation
-  renderer: 'svg',
-  loop: true,
-  autoplay: true,
-  path: 'js/data.json' // the path to the animation json
-  });
+  // lottie.loadAnimation({
+  // container: document.getElementById('bm'), // the dom element that will contain the animation
+  // renderer: 'svg',
+  // loop: true,
+  // autoplay: true,
+  // path: 'js/data.json' // the path to the animation json
+  // });
 
   var didScroll;
   var lastScrollTop = 0;
