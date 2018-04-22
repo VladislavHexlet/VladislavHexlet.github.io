b420b507171977ea9d379a07b3f8d289f9aa8d6a
anastasiaprh <anastasiyaprh@gmail.com>
2018-04-22 20:46:48 +0300

    update

--- js/main.js
+++ js/main.js
@@ -149,7 +149,7 @@ $(function() {
     var timeline = new TimelineMax();
 
     var tween1 = TweenMax.to(triggerElement + ' .process-textbox', 0.01, {css:{className:"+=process-text-animated"}});
-    var tween2 = TweenMax.fromTo(triggerElement + ' .process-text', 1, {autoAlpha:0}, {autoAlpha:1});
+    var tween2 = TweenMax.fromTo(triggerElement + ' .process-text', 1, {autoAlpha:0}, {autoAlpha:1, delay:1});
     // var tween3 = TweenMax.fromTo(triggerElement + ' .pt-text', 1, {autoAlpha:0}, {autoAlpha:1, delay:1});
     var tween4 = TweenMax.fromTo('#bm', 1, {autoAlpha:0}, {autoAlpha:0.5});
 
